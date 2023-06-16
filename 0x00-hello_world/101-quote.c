78ac1b57e5f1d1e07612978e3e7fd3200e0cc78f
ChadZombi <ChadMZA@gmail.com>
2023-06-16 07:33:08 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,8 @@
 int main(void)
 {
 	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	syscall(SYS_write, STDERR_FILENO, message, 43);
+	size_t len = 59;  /* Length of the message */
+
+	syscall(SYS_write, STDERR_FILENO, message, len);
 	return (1);
 }
