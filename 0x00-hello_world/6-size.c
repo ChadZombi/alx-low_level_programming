ee4dbb5bb20c858548989111c5753d82f24dd197
ChadZombi <ChadMZA@gmail.com>
2023-06-16 06:02:06 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,8 @@
  * Return: always 0 to show success
  */
 
-int main() {
+int main() 
+{
     printf("Size of a char: %lu byte(s)\n", sizeof(char));
     printf("Size of an int: %lu byte(s)\n", sizeof(int));
     printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
