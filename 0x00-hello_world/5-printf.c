a27b38d86c59a11ff04a7d3caf2793e13edb1021
ChadZombi <ChadMZA@gmail.com>
2023-06-16 05:19:38 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
 /**
  * main- entry point
  *
- * This function uses printf to print "with proper grammar, but the outcome is a piece of art,"
+ * This function uses printf to print
+ * "with proper grammar, but the outcome is a piece of art,"
  * followed by a new line
  *
  * return- always 0 showing success
