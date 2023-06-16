4793d2890696682c59ddf31fcb8b66e69f6b3539
ChadZombi <ChadMZA@gmail.com>
2023-06-16 04:44:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the string "Programming is like building
+ * a multilingual puzzle" followed by a new line using the `puts` function.
+ *
+ * Return: Always 0 (indicating success)
+ */
+
 int main(){
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
