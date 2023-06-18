150a6f493ff5d7d1455d63ad00f43c818992087e
ChadZombi <ChadMZA@gmail.com>
2023-06-18 08:28:52 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,16 +10,11 @@
  */
 int main(void)
 {
-	putchar('0' + 0);
-	putchar('1' + 0);
-	putchar('2' + 0);
-	putchar('3' + 0);
-	putchar('4' + 0);
-	putchar('5' + 0);
-	putchar('6' + 0);
-	putchar('7' + 0);
-	putchar('8' + 0);
-	putchar('9' + 0);
+	int number;
+
+	for (number = 0; number < 10; number++)
+		putchar(number + '0');
+
 	putchar('\n');
 
 	return (0);
