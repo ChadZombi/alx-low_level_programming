519efd0a5dde738e0602ce433d98ea054b0dc6ff
ChadZombi <ChadMZA@gmail.com>
2023-06-20 15:25:27 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - is used to search for lowercase
+ *
+ * @c: the charactor being searched
+ *
+ * Return: always 1 if true, otherwise 0
+ */
+
+int _islower(int c);
+{
+	if (c = [[:lower:]])
+		return (1);
+	return (0);
+}
