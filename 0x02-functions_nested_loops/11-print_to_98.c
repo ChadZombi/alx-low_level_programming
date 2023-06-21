4aa016363d43590787c949317a4c15651f59643c
ChadZombi <ChadMZA@gmail.com>
2023-06-20 18:16:34 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,25 +7,13 @@
  */
 void print_to_98(int n)
 {
-	if (n <= 98)
+	while (n != 98)
 	{
-		for (; n <= 98; n++)
-		{
-			printf("%d", n);
-			if (n != 98)
-				printf(",");
-			printf(" ");
-		}
+		printf("%d, ", n);
+		if (n > 98)
+			n--;
+		else
+			n++;
 	}
-	else
-	{
-		for (; n >= 98; n--)
-		{
-			printf("%d", n);
-			if (n != 98)
-				printf(",");
-			printf(" ");
-		}
-	}
-	printf("\n");
+	printf("98\n");
 }
