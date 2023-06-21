777a833bba7dda735ed1e61bdc27b71fcffe14d2
ChadZombi <ChadMZA@gmail.com>
2023-06-20 17:54:00 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - prints the multiplication of 9
+ *
+ * Return: always (0)
+ */
+
+void times_table(void)
+{
+	int x, y, result;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar('0');
+
+		for (y = 1; y <= 9; y++)
+		{
+			_putchar(',');
+
+			result = x * y;
+
+			if (result <= 9)
+				_putchar(' ');
+			else
+				_putchar((result / 10) + '0');
+
+			_putchar((result % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
