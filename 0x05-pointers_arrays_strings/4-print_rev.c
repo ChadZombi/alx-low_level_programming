2045e52e9e14cab739084f2a2ae8b95086c44356
ChadZombi <ChadMZA@gmail.com>
2023-06-26 15:12:16 -0700

    4-print-rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+#include <unistd.h>
+/**
+ * print_rev - prints a string in reverse
+ * @s: is the contains the string
+ * Return:void
+ */
+
+void print_rev(char *s)
+{
+int i;
+int length = 0;
+if (s == NULL)
+return;
+while (s[length] != '\0')
+{
+length++;
+}
+for(i = length - 1; i > 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
