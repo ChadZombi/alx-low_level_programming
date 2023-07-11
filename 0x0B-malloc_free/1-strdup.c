41dc32b9d20956acfefd3f8ca04983a896a8cdb1
ChadZombi <ChadMZA@gmail.com>
2023-07-11 07:37:09 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,21 +14,24 @@ char *_strdup(char *str)
 {
 char *nstr;
 unsigned int len, i;
+
 if (str == NULL)
 {
 return (NULL);
 }
+
 len = 0;
 while (str[len] != '\0')
 {
 len++;
 }
-nstr = malloc(sizeof(char) * (len = 1));
+nstr = malloc(sizeof(char) * (len + 1));
 
 if (nstr == NULL)
 {
 return (NULL);
 }
+
 for (i = 0; i < len; i++)
 {
 nstr [i] = str[i];
